1c4829b74c45b2d91eef5e778747d28bfaa53bc8
Dave G <dgoodrich@hbs.edu>
2016-01-04 13:01:55 -0500

    object is polls

--- graham-polygon/poly.js
+++ graham-polygon/poly.js
@@ -13,7 +13,7 @@ function renderPolyOptions() {
 	console.log ("render says: polys empty is " + (polys === void 0).toString() );
 
 	// if polys is empty, fetch the data
-	if (Object.keys(obj).length === 0) {
+	if (Object.keys(polys).length === 0) {
 		fetchPolys();
 	}
 
